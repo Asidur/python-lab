c28386093dec4b4431fbd9c2258f4ac1618dbc8e
ASIDUR RAHMAN <asidurrahman48@gmail.com>
2025-04-09 18:51:44 +0530

    Add files via upload

---
+++ week-3/06_sum_command_args.py
@@ -0,0 +1,7 @@
+import sys
+if len(sys.argv) != 3:
+    print("Usage: python script.py num1 num2")
+else:
+    num1 = int(sys.argv[1])
+    num2 = int(sys.argv[2])
+    print("Sum:", num1 + num2)
---
+++ week-3/07_operator_usage.py
@@ -0,0 +1,24 @@
+a = 10
+b = 5
+print("Arithmetic Operators:")
+print("a + b =", a + b)
+print("a - b =", a - b)
+print("a * b =", a * b)
+print("a / b =", a / b)
+print("a % b =", a % b)
+
+print("\nRelational Operators:")
+print("a > b:", a > b)
+print("a < b:", a < b)
+
+print("\nLogical Operators:")
+print("a > 5 and b < 10:", a > 5 and b < 10)
+print("a < 5 or b < 10:", a < 5 or b < 10)
+
+print("\nAssignment Operators:")
+a += 1
+print("a += 1:", a)
+
+print("\nBitwise Operators:")
+print("a & b:", a & b)
+print("a | b:", a | b)
